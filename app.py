b41f749eca0f8549e567dde6d0094f807e9c206b
Omar Boudellah <boudellah1.o@gmail.com>
2025-07-06 23:49:46 +0100

    add route

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'Index Page'
 
-from markupsafe import escape
-
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
 
 if __name__ == "__main__":
     app.run(debug=True)
