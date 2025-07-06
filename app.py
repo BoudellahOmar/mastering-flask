80bbc63262f24217cd97b3ae5bee37b7ced10546
Omar Boudellah <boudellah1.o@gmail.com>
2025-07-07 00:23:38 +0100

    add get url of pages

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+from flask import url_for
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'index'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/login')
+def login():
+    return 'login'
 
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 if __name__ == "__main__":
     app.run(debug=True)
